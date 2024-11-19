a86dec923b01bd8e5477d645826dff97206a724f
precious654 <odukwechiemeka@gmail.com>
2024-11-19 06:02:36 +0100

    implemented signin with credentials, finished handling authentication

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,5 +1,8 @@
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/app/(models)/User";
+import bcrypt from "bcrypt";
 
 export const options = {
   providers: [
@@ -10,6 +13,39 @@ export const options = {
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email:",
+          type: "email",
+          placeholder: "Your Email",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "Your Password",
+        },
+      },
+      async authorize(credentials) {
+        try {
+          const user = await User.findOne({ email: credentials.email })
+            .lean()
+            .exec();
+          if (user) {
+            console.log(user.email);
+            const match = bcrypt.compare(credentials.password, user.password);
+            if (match) {
+              delete user.password;
+              return user;
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+        return null;
+      },
     }),
   ],
   callbacks: {},
